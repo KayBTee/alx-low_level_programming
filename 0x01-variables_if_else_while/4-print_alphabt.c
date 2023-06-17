c4e8d43b146125c274da51a31abeb5ee4d24d19a
KayBTee <kbthaba@gmail.com>
2023-06-17 11:20:39 -0700

    When I was having that alphabet soup

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - Entry Point
- * Description 'print alphabets except q and e'
+ * Description: 'print alphabets except q and e'
  * Return: Always 0
  */
 int main(void)
