ff87f4a2d4063801ecf733a54e88996697f4ccc3
KayBTee <kbthaba@gmail.com>
2023-06-20 09:20:12 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _islower - shows 1 if the input is a lowercase and returns 0 in other cases
+ *
+ * @c: The character is ASCII code
+ *
+ * Return: 1 for lowercase and 0 for other cases
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
