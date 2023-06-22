f9e379ab9f125fd3661369def212da1676ce8672
KayBTee <kbthaba@gmail.com>
2023-06-22 14:20:32 -0700

    Triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle
+ * @size: ize of the triangle
+ * Return: Always 0
+ */
+
+void print_triangle(int size)
+{
+	int row, hash, space;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (row = 1; row <= size; row++)
+		{
+			for (space = size - row; space >= 1; space--)
+			{
+				_putchar(' ');
+			}
+			for (hash = 1; hash <= row; hash++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
