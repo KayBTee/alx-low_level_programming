59333d9dfe81eafe2a7a2fd6e9501482d87967a2
KayBTee <kbthaba@gmail.com>
2023-06-26 16:01:11 -0700

    I can only go one way

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,16 +1,14 @@
 #include "main.h"
-#include <string.h>
 
 /**
- * print_rev - prints a string in reverse
- * @s:string being reversed
+ * rev_string - a function that reverses a string
+ * @s:character being reversed
  */
 
-void print_rev(char *s)
+void rev_string(char *s)
 {
 	int length = strlen(s);
 	int i = length - 1;
-
 	while (i >= 0)
 	{
 		_putchar(s[i]);
