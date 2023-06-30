a22d78bd274f998fe2bbb031fbb8a6eb83954fe3
KayBTee <kbthaba@gmail.com>
2023-06-29 18:16:07 -0700

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,7 +1,30 @@
 #include "main.h"
 
-/** *rot13 - encodes a string using rot13.
- * Return:
+/**
+ * *rot13 - encodes a string
+ * @str: string being checked
+ * Return: str
  */
 
-char *rot13(char *)
+char *rot13(char *str)
+{
+	int i;
+	int j;
+
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (str[i] == data1[j])
+			{
+				str[i] = datarot[j];
+				break;
+			}
+		}
+	}
+	return (str);
+}
