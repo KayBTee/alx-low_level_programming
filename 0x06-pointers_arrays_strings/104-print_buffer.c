be325421ed307a675ca760b88aa1c24b4016b7a2
KayBTee <kbthaba@gmail.com>
2023-06-30 10:44:02 -0700

    A dream doesnt become reality through magic

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,10 +1,45 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * print_buffer - prints a buffer
  * @b: pointing to the buffer
  * @size: contents being printed
- * Return:
+ * Return: Always 0
  */
-
 void print_buffer(char *b, int size)
+{
+	if (size <= 0) {
+		printf("\n");
+		return;
+	}
+	int i, j;
+
+	for (i = 0; i < size; i += 10) {
+		printf("%08x ", i);
+
+		for (j = i; j < i + 10; j += 2) {
+			if (j < size) {
+				printf("%02x", (unsigned char)b[j]);
+
+				if (j + 1 < size)
+					printf("%02x", (unsigned char)b[j + 1]);
+				else {
+					printf("  ");
+				}
+			} else {
+				printf("    ");
+			}
+			printf(" ");
+		}
+		printf(" ");
+
+		for (j = i; j < i + 10 && j < size; j++)
+			if (isprint(b[j]))
+				printf("%c", b[j]);
+			else {
+				printf(".");
+			}
+		}
+		printf("\n");
+	}
+}
