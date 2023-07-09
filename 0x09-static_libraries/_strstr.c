7b7137605856049ab9dfdb6af11af7773901f48c
KayBTee <kbthaba@gmail.com>
2023-07-09 11:32:19 -0700

    A library is not a luxury but one of the necessities of life

---
+++ 0x09-static_libraries/_strstr.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * *_strstr - locates a suubstring
+ * @haystack: the string to be searched
+ * @needle: the substring to be locate
+ * Return: a pointer
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+        int ind;
+
+        if (*needle == 0)
+                return (haystack);
+        while (*haystack)
+        {
+                ind = 0;
+
+                if (haystack[ind] == needle[ind])
+                {
+                        do {
+                                if (needle[ind + 1] == '\0')
+                                        return (haystack);
+
+                                ind++;
+                        } while (haystack[ind] == needle[ind]);
+                }
+                haystack++;
+        }
+        return ('\0');
+}
