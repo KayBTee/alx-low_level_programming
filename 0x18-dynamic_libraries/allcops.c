f2907f1973a787db360aea2b83b6b4aa1b3ac3d6
KayBTee <kbthaba@gmail.com>
2024-01-15 19:43:02 +0000

    2Lets call C functions from Python

---
+++ 0x18-dynamic_libraries/allcops.c
@@ -0,0 +1,54 @@
+/**
+ * add - adds two numbers
+ * @a: first int
+ * @b: second int
+ * Return: final answer
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtracts two numbers
+ * @a: first int
+ * @b: second int
+ * Return: final answer
+ */
+int sub(int a, int b)
+{
+        return (a - b);
+}
+
+/**
+ * mul - multiplies two numbers
+ * @a: first int
+ * @b: second int
+ * Return: final answer
+ */
+int mul(int a, int b)
+{
+        return (a * b);
+}
+
+/**
+ * div - divides two numbers
+ * @a: first int
+ * @b: second int
+ * Return: final answer
+ */
+int div(int a, int b)
+{
+        return (a / b);
+}
+
+/**
+ * mod - modulus of two numbers
+ * @a: first int
+ * @b: second int
+ * Return: final answer
+ */
+int mod(int a, int b)
+{
+        return (a % b);
+}
